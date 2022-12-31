66551e3b1a3121b7a7625bef2d64d94e9ac7d2d2
CV . Lawi3 Cemerlang <64375997+Hanik563@users.noreply.github.com>
2022-12-31 21:44:48 +0700

    Update gas.php

--- gas.php
+++ gas.php
@@ -1,32 +1,152 @@
 <?php
 ///////////////////////////////////////////
-///////CREATED BY ARUDJI HERMATYAR////////
-//////www.facebook.com/bantalku567///////
-/////https://github.com/arudji1211//////
+///////CREATED BY MAKLO GAMING////////
+            /////////////
+/////https://github.com/Hanik563//////
 ///////////////////////////////////////
 
-include 'tri_req.php';
+/*
 
-$tri = new tri();
-$imei = "868880043302499";
-echo "Masukkan No Telepon : ";
-$msisdn = trim(fgets(STDIN));
-$otp = $tri->request_otp($msisdn,$imei);
-echo $otp[1] . "\r\n";
-echo "Masukkan OTP : ";
-$otp = trim(fgets(STDIN));
-$login = $tri->login($msisdn,$otp);
-$login = json_decode($login,true);
-$bearer = $login['access_token'];
-$id = $tri->trans($bearer);
-$id = json_decode($id,true);
-$id = $id['data'][0]['rewardTransactionId'];
-for($id1 = 1500; $id1 < 1600;$id1++)
-{
-  $gas = $tri->claim($bearer,$id,$id1);
-  echo $gas . "\r\n";
-  sleep(2);
+function sendOTP($pnum){
+$url = "https://api-wco.linkaja.id/v1/wco/partner/inquiry HTTP/1.1
+";
+$data = '{"csrvalue":"'.$trx-id.'"}';
+$milliseconds = floor(microtime(true) * 1000);
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$headers = array(
+   "user-agent: Dart/2.17 (dart:io)",
+   "x-signature: 1663685232458:e75c544db1782228865f27a2f1fed11acaad1308587538bdf7546ac104fe4d3c",
+   "accept-language: en",
+   //"accept-encoding: gzip",
+   "content-length: ".strlen($data),
+   "host: api2.sbuxcard.com",
+   "Content-Type: application/json",
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+//for debug only!
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+return $resp;
+
 }
 
+function Trx-id($trx-id){
+$data = '{"notrx":"'.$trx-id.'"}';
 
-?>
+$url = "https://www.mcdelivery.co.id/id/tcash-intermediate.html";
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$headers = array(
+   "user-agent: Dart/2.17 (dart:io)",
+   "x-signature: 1663685259200:1e4bc50096b733dab0ff47df665c4d0f571dc8eb8f2728b9846473acf160170b",
+   "accept-language: en",
+   //"accept-encoding: gzip",
+   "content-length: ".strlen($data),
+   "host: api-wco.linkaja.id",
+   "Content-Type: application/json",
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+//for debug only!
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+return $resp;
+
+}
+
+function register($email,$phone,$otpku,$nama1,$nama2){
+$url = "https://payment.linkaja.id/checkout/payment";
+
+$data = '{"{"data":{},"status":"00","message":"code=404, message=Inquiry success"}"}';
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$headers = array(
+   "user-agent: Dart/2.17 (dart:io)",
+   "x-signature: 1663685319338:0b8592b21c9b6448e3fa674f206ecd9750ed77e2b91d09ad2c970a6b2c79dba7",
+la   "accept-language: en",
+   //"accept-encoding: gzip",
+   "content-length: ".strlen($data),
+   "host: api-wco.linkaja.id",
+   "Content-Type: application/json",
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+//for debug only!
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+return $resp;
+
+}
+
+function getName()
+    {
+        $r = anjinglah('https://www.random-number-generator.com/indonesia?gender=&n=1&s='.rand(111,999));
+        $namenya = get_between($r,'<div class="col-sm-12 mb-3" id="','-');
+$number_indo = preg_replace('/s+/', '', $namenya);
+       return ucfirst($number_indo);
+    }
+	
+	function get_between($string, $start, $end) {
+    $string = " ".$string;
+    $ini = strpos($string,$start);
+    if ($ini == 0) return "";
+    $ini += strlen($start);
+    $len = strpos($string,$end,$ini) - $ini;
+    return substr($string,$ini,$len);
+}
+ method:"POST",
+		            success: function(response, status, xhr) {
+						// success, we can submit the form
+	                    if(response.message=='success' && typeof(response.code)=='undefined'){
+	                    	 console.log('success');
+	                         $('#form_fb_linkaccount_modal').hide();
+	                          error.empty().hide();
+	                          error.parents('.form-group').removeClass('form-group-error');
+	                          window.location.replace(response.redirectUrl);
+	                    }else{
+	                    	 $('#form_fb_linkaccount_modal').show();
+	                            error.html(response.message).show();
+	                            error.parents('.form-group').addClass('form-group-error');
+	                    }
+
+function anjinglah($Url) {
+    if (!function_exists('curl_init')){ 
+        die('CURL is not installed!');
+    }
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $Url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $output = curl_exec($ch);
+    curl_close($ch);
+    return $output;
+}
